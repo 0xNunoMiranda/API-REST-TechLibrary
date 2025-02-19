e583e6ba08b0ab872ae9393294368c70a1e7fb17
Nuno Miranda <nuno_miranda@outlook.com>
2025-02-19 12:16:51 +0000

    Add project files.

---
+++ TechLibrary.Api/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TechLibrary.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        public IActionResult Get()
+        {
+            return Ok("Get all users");
+        }
+
+
+    }
+}
