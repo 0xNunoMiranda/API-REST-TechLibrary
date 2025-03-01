9a3d66fc7bb139332c44e935b1e54c5f139ae378
Nuno Miranda <71593925+0xNunoMiranda@users.noreply.github.com>
2025-03-01 17:28:18 +0000

    token generator fixed

---
+++ TechLibrary.Api/Infrastructure/Security/Token/Access/TokenJwtGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace TechLibrary.Api.Infrastructure.Security.Token.Access;
+
+public class TokenJwtGenerator
+{
+    public string GenerateToken(string email)
+    {
+        var tokenDescriptor = new SecurityTokenDescriptor()
+        {
+            Expires = DateTime.UtcNow.AddHours(24 * 7), // 7 days
+            SigningCredentials = new SigningCredentials(SecurityKey(), SecurityAlgorithms.HmacSha256Signature),
+        };
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(securityToken);
+    }
+
+    private static SymmetricSecurityKey SecurityKey()
+    {
+        return new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mkj2h34h2342h3d23x43ioczcfdg347963453450df"));
+    }
+}
--- TechLibrary.Api/UseCases/Users/Register/RegisterUserUseCase.cs
+++ TechLibrary.Api/UseCases/Users/Register/RegisterUserUseCase.cs
@@ -1,37 +1,48 @@
-﻿using TechLibrary.Api.Domain.Entities;
-using TechLibrary.Api.Infrastructure;
+﻿using FluentValidation.Results;
+using TechLibrary.Api.Domain.Entities;
+using TechLibrary.Api.Infrastructure.DataAccess;
+using TechLibrary.Api.Infrastructure.Security.Criptography;
 using TechLibrary.Communication.Requests;
 using TechLibrary.Communication.Responses;
 using TechLibrary.Exception;
-
+using TechLibrary.Api.Infrastructure.Security.Token.Access;
 namespace TechLibrary.Api.UseCases.Users.Register;
 
 public class RegisterUserUseCase
 {
     public ResponseRegisterUserJson Execute(RequestUserJson request)
     {
-        Validate(request);
+        var dbContext = new TechLibraryDbContext();
+
+        Validate(request, dbContext);
+        
+        var cryptography = new BCryptAlgorithm();
 
         var entity = new User
         {
             Name = request.Name,
             Email = request.Email,
-            Password = request.Password
+            Password = cryptography.HashPassword(request.Password)
         };
 
-        var dbContext = new TechLibraryDbContext();
         dbContext.Users.Add(entity);
         dbContext.SaveChanges();
+
+        var tokenGenerator = new TokenJwtGenerator();
+
         return new ResponseRegisterUserJson
         {
             Name = entity.Name,
         }; 
     }
 
-    private void Validate(RequestUserJson request)
+    private void Validate(RequestUserJson request, TechLibraryDbContext dbContext)
     {
         var validator = new RegisterUserValidator();
         var result = validator.Validate(request);
+
+        var existUser = dbContext.Users.Any(user => user.Email.ToLower().Trim() == request.Email.ToLower().Trim());
+        result.Errors.Add(new ValidationFailure("Email","Email already registered."));
 
         if (result.IsValid == false)
         {
